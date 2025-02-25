d2763e1fdd61a2915e0003573ed87354b5b88484
Oleksandr Merkulov <oleksandrmerkuloff@gmail.com>
2025-02-25 03:02:42 +0200

    created user credits view set

--- api/views.py
+++ api/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework import viewsets, status
+from rest_framework.response import Response
+from django.shortcuts import get_object_or_404
 
-# Create your views here.
+from .models import User, Credit, Plan, Payment, Dictionary
+from .serializers import UserSerializer, CreditSerializer, PlanSerializer, PaymentSerializer, DictionarySerializer
+
+
+class UserCreditsViewSet(viewsets.ViewSet):
+    def list(self, request, user_id=None):
+        user = get_object_or_404(User, pk=user_id)
+        credits = user.credits.all()
+        serializer = CreditSerializer(credits, many=True)
+        return Response(serializer.data)
